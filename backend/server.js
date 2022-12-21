1beb7e3682aa65b103b579813f7fbebc13c47adf
jpp <jpnamausa@gmail.com>
2022-12-21 11:14:58 -0500

    modified:   backend/middleware/errorMiddleWare.js 	modified:   backend/server.js 	modified:   backend/controller/goalController.js 	backend/models/

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ connectDB();
 const app = express();
 
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use('/api/goals', require('./routes/getroutes.js'))
 app.use(errorHandler)
 
