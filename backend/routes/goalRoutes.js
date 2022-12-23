a2e1bd2568af5ed1ee169865e6fdfbe887628a13
Jason Preneveau <37848207+runtimejpp@users.noreply.github.com>
2022-12-22 22:26:22 -0500

    "some updates"

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const router = express.Router()
+
+const {
+   getGoals, 
+   setGoal,
+   updateGoal,
+   deleteGoal 
+} = require('../controller/goalController')
+
+router.route('/').get(getGoals).post(setGoal)
+router.route('/:id').delete(deleteGoal).put(updateGoal)
+
+router.get('/',getGoals)
+router.post('/', setGoal)
+router.put('/:id',updateGoad)
+router.delete('/:id', deleteGoal)
+
+
+module.exports = router 
+
