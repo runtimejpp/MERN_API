dba74e851399181f99d7c6f56deb39380ff11b4e
Jason Preneveau <37848207+runtimejpp@users.noreply.github.com>
2022-12-26 19:49:29 -0500

    updates

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,21 +1,15 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
-
 const {
-   getGoals, 
-   setGoal,
-   updateGoal,
-   deleteGoal 
-} = require('../controller/goalController')
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require('../controllers/goalController')
 
-router.route('/').get(getGoals).post(setGoal)
-router.route('/:id').delete(deleteGoal).put(updateGoal)
+const { protect } = require('../middleware/authMiddleWare')
 
-router.get('/',getGoals)
-router.post('/', setGoal)
-router.put('/:id',updateGoad)
-router.delete('/:id', deleteGoal)
+router.route('/').get(protect, getGoals).post(protect, setGoal)
+router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal)
 
-
-module.exports = router 
-
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,34 @@
-console.log("Hey Buddy")
-const colors = require('colors')
-const express = require('express')
-const dotenv = require('dotenv').config()
-const port = process.env.PORT || 5000
-const nodemon = require('nodemon')
+const path = require('path');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleWare');
+const connectDB = require('./config/db')
+const port = process.env.PORT || 5000;
 
-const { errorHandler } = require('./middleware/errorMiddleWare')
-const connectDB = require(`./config/db`)
 connectDB();
+
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use('/api/goals', require('./routes/goalRoutes.js'))
-app.use(errorHandler)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.listen(port, () => console.log('server started on ${port}'))
+app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
 
+app.use(errorHandler);
 
-
-
-
+app.listen(port, () => console.log(`Server started on port ${port}`));
