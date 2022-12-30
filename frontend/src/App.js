ae5704a2fd48457dfd4d7ceeca0a6f4ea229101a
Jason Preneveau <37848207+runtimejpp@users.noreply.github.com>
2022-12-29 20:41:44 -0500

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
+
+function App() {
+  return (
+    <>
+    <div>
+    <h1>my app</h1>
+    </div>
+    </>
+  );
+}
+
+export default App;
